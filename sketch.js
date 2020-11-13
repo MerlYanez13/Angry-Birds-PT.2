85207f49e103433fd2a8d6ef3e5f5b1080604f22
MerlYanez13 <71855710+MerlYanez13@users.noreply.github.com>
2020-11-12 19:31:24 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
-var bg;
+var backgroundImg;
 
-function preload(){
-    bg=loadImage("sprites/bg.png");
-
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
 }
 
 function setup(){
@@ -16,7 +16,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
+    platform=new Ground (150,305,300,170)
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -34,13 +34,16 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    log6= new Log (230,180,80,PI/2);
+
+    
     bird = new Bird(100,100);
+    slingshot= new  Slingshot (bird.body,log6.body);
 
 }
 
 function draw(){
-    
-    background(bg);
+    background(backgroundImg);
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
@@ -59,6 +62,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
+    slingshot.display();
     bird.display();
+    platform.display();
 }
